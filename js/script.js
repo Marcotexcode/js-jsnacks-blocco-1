19d43e273726a5dc9917b457875509350bd58650
Marco Tessitore <marcotexcode@gmail.com>
2021-06-01 14:06:01 +0200

    INIT

---
+++ js/script.js
@@ -0,0 +1,53 @@
+
+
+
+// // ************** ESERCIZIO 1 ************** //
+
+// L’utente inserisce due parole in successione, con due prompt. 
+// Il software stampa prima la parola più corta, poi la parola più lunga.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // ************** ESERCIZIO 2 ************** //
+
+// Il software deve chiedere per 5 volte all’utente di inserire un numero. 
+// Il programma stampa la somma di tutti i numeri   inseriti. 
+// Esegui questo programma in due versioni, con il for e con il while.
+
+
+
+
+
+//  // ************** ESERCIZIO 3 ************** //
+
+// Crea un array vuoto. 
+// Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array
+
+
+
+
+//  // ************** ESERCIZIO 4 ************** //
+
+
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, 
+// chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
