e5bb18be64c1660c13d0799fee1f6a4d04a59575
Marco Tessitore <marcotexcode@gmail.com>
2021-06-01 14:17:05 +0200

    add: esercizio 1

--- js/script.js
+++ js/script.js
@@ -7,22 +7,22 @@
 // Il software stampa prima la parola più corta, poi la parola più lunga.
 
 
+var  parola1 = prompt('inserisci prima parola');
+var  parola2 = prompt('inserisci la seconda parola');
 
 
+if (parola1.length < parola2.length) {
 
+    console.log(parola1 + ' ' + parola2);
 
+}else if (parola1.length === parola2.length) {
 
+    console.log('sono uguali ' + parola2 + ' ' + parola1);
 
-
-
-
-
-
-
-
-
-
-
+}else {
+    
+    console.log(parola2 + ' ' + parola1);
+}
 
 
 
